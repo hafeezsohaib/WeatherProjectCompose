893dc1407a0940740ec39096df7fabc958195a68
suhebkhan <suheb.khan7@gmail.com>
2023-10-27 10:40:14 +0530

    Dev3 second commit PR

--- app/src/main/java/com/suheb/weatherappcompose/common/Constant.kt
+++ app/src/main/java/com/suheb/weatherappcompose/common/Constant.kt
@@ -5,7 +5,7 @@ import android.icu.text.DecimalFormat
 class Constant {
 
     companion object{
-       val a:String="Dev3 code to Dev2...."
+       val a:String="Dev3 code to Dev1...."
         val b:String="taken......"
         const val Base_URL="https://api.openweathermap.org/data/2.5/"
 
